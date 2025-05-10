73fc166f3c6de0d79c99e3a4c7f5f2e750b50a79
qq1442104999 <qq1442104999@qq.com>
2025-05-10 22:14:18 +0800

    0

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,2 @@
-require('profile')  -- 基础配置
-require('packinit') -- 插件配置
-require('keymap')   -- 按键配置
-require('autocmd')  -- 自动命令配置
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
